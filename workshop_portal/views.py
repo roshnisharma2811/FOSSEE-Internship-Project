c277ebd3a8b9d09d0b4b77f54aa456ba0de0682b
roshnisharma2811 <roshnis1127@gmail.com>
2025-09-13 15:59:48 +0530

    Initial commit-FOSSEE task 1

--- workshop_portal/views.py
+++ workshop_portal/views.py
@@ -1,16 +1,6 @@
 # Django Imports
-from django.shortcuts import redirect
-from django.urls import reverse
-from django.conf import settings
-
-# Local Imports
-from cms.models import Page
+from django.shortcuts import render
 
 
 def index(request):
-    page = Page.objects.filter(title=settings.HOME_PAGE_TITLE)
-    if page.exists():
-        redirect_url = reverse("cms:home", args=[page.first().permalink])
-    else:
-        redirect_url = reverse("workshop_app:index")
-    return redirect(redirect_url)
+    return render(request, "workshop_app/index.html")
